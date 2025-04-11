8443b37421f7e5b181079d089bfe6d168793c5cf
andreaklcortez <andreakristina.cortez@sjsu.edu>
2025-04-11 09:21:59 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -75,7 +75,7 @@
 `define B5  16 // 987.76 Hz 
 
 
-module tt_sjsu_vga_music(
+module tt_um_sjsu_vga_music(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
