dc56f8afbdaa2545b85145a0ab9b3440d98ceb7c
andreaklcortez <andreakristina.cortez@sjsu.edu>
2025-09-13 19:08:08 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_sjsu_vga_music andreaklcortez (
+  tt_um_sjsu_vga_music andreaklcortez (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
